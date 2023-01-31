9eec5f4fc4afaebe7f99fb676a1e37b830d71d56
fc1943s <root@fc1943s.dev>
2023-01-31 08:43:49 -0300

    tests

--- rust/src/tests.rs
+++ rust/src/tests.rs
@@ -2,10 +2,10 @@
 pub mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     // use super::*;
-    use super::super::*;
+    // use super::super::*;
 
-    #[test]
-    fn test_two_sum() {
-        assert_eq!(main::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
-    }
+    // #[test]
+    // fn test_two_sum() {
+    //     assert_eq!(main::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+    // }
 }
