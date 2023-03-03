42b65e65416856b7914b8e516e59eabffe590fcb
fc1943s <root@fc1943s.dev>
2023-03-03 09:15:22 -0300

    tests

--- init.ps1
+++ init.ps1
@@ -1,6 +1,7 @@
 # rustup +nightly target add wasm32-unknown-unknown
 # rustup +nightly target add wasm32-wasi
 rustup +nightly component add rust-src
+rustup component add rustfmt
 
 # cargo install wasm-pack
 
