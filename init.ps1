be87ce716bd31620f0116451482ea5eb021f2a51
프리마 <fc1943s@pm.me>
2023-04-05 10:35:21 +0000

    tests

--- init.ps1
+++ init.ps1
@@ -1,3 +1,6 @@
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# source "$HOME/.cargo/env"
+
 # rustup +nightly target add wasm32-unknown-unknown
 # rustup +nightly target add wasm32-wasi
 rustup +nightly component add rust-src
