6d55b1894254bc51537ebbf8ce76e7f05d8a3f19
프리마 <fc1943s@pm.me>
2023-06-07 01:50:16 -0300

    tests

--- init.ps1
+++ init.ps1
@@ -1,35 +1,22 @@
 $ErrorActionPreference = "Stop"
 . ./core.ps1
 
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-# source "$HOME/.cargo/env"
-# export PATH="$HOME/.cargo/bin:$PATH"
-
-# rustup +nightly target add wasm32-unknown-unknown
-# rustup +nightly target add wasm32-wasi
-
-rustup update nightly
 
 rustup +nightly component add clippy rust-src rustfmt
+rustup update
 
-rustup update nightly
+cargo +nightly install wasm-pack
 
-$cargoCommand = 'cargo +nightly install wasm-pack --keep-going -Z unstable-options';
-if ($env:CI) { Invoke-Expression "$cargoCommand | Out-Null" } else { Invoke-Expression $cargoCommand }
 
 npm install -g esbuild
+
 
 dotnet tool restore
 dotnet paket restore
 
-# echo 'set -gx PATH $HOME/.cargo/bin $PATH' >> ~/.config/fish/config.fish
-
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-# Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser
 Install-Module -Name PowerShellGet -Force -AllowClobber -Scope CurrentUser
-
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
 Install-Module -Name PSToml -Scope CurrentUser
