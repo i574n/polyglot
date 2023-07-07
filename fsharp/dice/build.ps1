c0387e75d78cbcc635df8fed226aa399873e8d4a
프리마 <fc1943s@pm.me>
2023-07-07 10:01:56 -0300

    tests

--- fsharp/dice/build.ps1
+++ fsharp/dice/build.ps1
@@ -1,4 +1,9 @@
-Set-Location $PSScriptRoot
+$ScriptDir = $PSScriptRoot
+Set-Location $ScriptDir
 $ErrorActionPreference = "Stop"
+. ../../scripts/core.ps1
+
 
 { dotnet run -c Release | Tee-Object dice.ans } | Invoke-Block
+
+# { dotnet repl --run dice.dib --output-path dice.repl.ipynb --exit-after-run } | Invoke-Block
