1764921742c2b294505e12832f73b66ad84e3488
프리마 <fc1943s@pm.me>
2023-07-17 04:56:29 -0300

    tests

--- scripts/symlinks.ps1
+++ scripts/symlinks.ps1
@@ -5,14 +5,19 @@ $ErrorActionPreference = "Stop"
 
 
 function EnsureSymbolicLink([string]$Path, [string] $Target) {
+    $Path =  [IO.Path]::GetFullPath((Join-Path $ScriptDir $Path))
+    $Target = [IO.Path]::GetFullPath((Join-Path $ScriptDir $Target))
+
     if (-Not (Test-Path $Path)) {
+        Write-Output "Creating symlink: $Path -> $Target"
         New-Item -ItemType SymbolicLink -Path $Path -Target $Target -ErrorAction SilentlyContinue | Out-Null
+    } else {
+        Write-Output "Symlink already exists: $Path -> $Target"
     }
 }
 
-$nbsTarget = Join-Path $ScriptDir "../nbs"
-EnsureSymbolicLink -Path "../nbs/nbs" -Target $nbsTarget
-EnsureSymbolicLink -Path "../scripts/nbs" -Target $nbsTarget
-EnsureSymbolicLink -Path "../apps/dice/nbs" -Target $nbsTarget
-EnsureSymbolicLink -Path "../apps/parser/nbs" -Target $nbsTarget
-EnsureSymbolicLink -Path "../apps/spiral/nbs" -Target $nbsTarget
+EnsureSymbolicLink -Path "../nbs/nbs" -Target "../nbs"
+EnsureSymbolicLink -Path "../scripts/nbs" -Target "../nbs"
+EnsureSymbolicLink -Path "../apps/dice/nbs" -Target "../nbs"
+EnsureSymbolicLink -Path "../apps/parser/nbs" -Target "../nbs"
+EnsureSymbolicLink -Path "../apps/spiral/nbs" -Target "../nbs"
