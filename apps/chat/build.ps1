424e191800a06e562e2e939fb446079f25c1d7d2
프리마 <fc1943s@pm.me>
2023-08-07 20:49:03 -0300

    tests

--- apps/chat/build.ps1
+++ apps/chat/build.ps1
@@ -12,7 +12,12 @@ Copy-Item -Force contract/target/wasm32-unknown-unknown/release/chat.wasm contra
 $nearSandboxExe = "tests/target/near-sandbox"
 if (!(Test-Path $nearSandboxExe)) {
     $nearSandboxZip = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath "near-sandbox.zip"
-    Invoke-WebRequest -Uri "https://bafybeiahopxwsng6nmgihipwjpv6nzwoz7ta2jam6bukve7kni5auhiimu.ipfs.dweb.link" -OutFile $nearSandboxZip
+    $url = if (Get-Content -Path /etc/os-release -ErrorAction SilentlyContinue | Select-String -Pattern "NixOS") {
+        "https://bafybeiahopxwsng6nmgihipwjpv6nzwoz7ta2jam6bukve7kni5auhiimu.ipfs.dweb.link"
+    } else {
+        "https://bafybeictjqjc3sbppcslzezn64h7acnrmt3oskdyjxgcrawlu6pujvin3i.ipfs.dweb.link"
+    }
+    Invoke-WebRequest -Uri $url -OutFile $nearSandboxZip
     Expand-Archive -Path $nearSandboxZip -DestinationPath "tests/target" -Force
     { chmod +x $nearSandboxExe } | Invoke-Block -Linux
 }
