672e4c2aa0c3b4d4a4236e1e073a4019c1d0dead
프리마 <fc1943s@pm.me>
2023-08-07 16:49:41 -0300

    tests

--- apps/chat/build.ps1
+++ apps/chat/build.ps1
@@ -9,4 +9,11 @@ $ErrorActionPreference = "Stop"
 { cargo build --release --target wasm32-unknown-unknown --manifest-path contract/Cargo.toml } | Invoke-Block
 Copy-Item -Force contract/target/wasm32-unknown-unknown/release/chat.wasm contract/res/chat.wasm
 
-{ cargo run --release --example tests --manifest-path tests/Cargo.toml } | Invoke-Block -Linux -OnError Continue -EnvironmentVariables @{ "NEAR_RPC_TIMEOUT_SECS" = 100; "NEAR_SANDBOX_BIN_PATH" = (($p = "../../../nearcore/target/debug/near-sandbox") -and (Test-Path $p) ? $p : "") }
+$nearSandboxExe = "tests/target/near-sandbox"
+if (!(Test-Path $nearSandboxExe)) {
+    $nearSandboxZip = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath "near-sandbox.zip"
+    Invoke-WebRequest -Uri "https://bafybeiahopxwsng6nmgihipwjpv6nzwoz7ta2jam6bukve7kni5auhiimu.ipfs.dweb.link" -OutFile $nearSandboxZip
+    Expand-Archive -Path $nearSandboxZip -DestinationPath "tests/target" -Force
+}
+
+{ cargo run --release --manifest-path tests/Cargo.toml } | Invoke-Block -Linux -EnvironmentVariables @{ "NEAR_RPC_TIMEOUT_SECS" = 100; "NEAR_SANDBOX_BIN_PATH" = $nearSandboxExe }
