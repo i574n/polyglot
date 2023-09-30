769bfd73290b8625f9606273384dcf9b6beaec43
프리마 <fc1943s@pm.me>
2023-09-30 01:10:29 -0400

    tests

--- apps/spiral/build.ps1
+++ apps/spiral/build.ps1
@@ -16,9 +16,9 @@ if (!$fast) {
 { . ../builder/dist/Builder$(GetExecutableSuffix) Supervisor.fs $($fast ? @("--runtime", ($IsWindows ? "win-x64" : "linux-x64")) : @()) --packages Argu FSharp.Control.AsyncSeq FSharp.Json Microsoft.AspNetCore.SignalR.Client System.CommandLine System.Reactive.Linq --modules nbs/Common.fs nbs/CommonFSharp.fs nbs/Threading.fs nbs/Async.fs nbs/AsyncSeq.fs nbs/Networking.fs nbs/Runtime.fs nbs/FileSystem.fs } | Invoke-Block
 
 if (!$fast) {
-    Invoke-Dib Spiral.dib
-    Invoke-Dib Physics.dib
-    Invoke-Dib Tasks.dib
+    Invoke-Dib Spiral.dib -Retries 3
+    Invoke-Dib Physics.dib -Retries 3
+    Invoke-Dib Tasks.dib -Retries 3
 }
 
 if ($env:CI) {
