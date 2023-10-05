f051e071957230b61e733f66527f513d87089ae4
프리마 <fc1943s@pm.me>
2023-10-04 20:03:08 -0400

    tests

--- lib/spiral/build.ps1
+++ lib/spiral/build.ps1
@@ -9,13 +9,15 @@ $ErrorActionPreference = "Stop"
 
 if (!$fast) {
     Invoke-Dib testing.dib
+    Invoke-Dib common.dib
     Invoke-Dib console.dib
     Invoke-Dib math.dib
     Invoke-Dib "optionm'.dib"
     Invoke-Dib "am'.dib"
     Invoke-Dib "listm'.dib"
+    Invoke-Dib seq.dib
     Invoke-Dib util.dib
     Invoke-Dib physics.dib
 }
 
-{ . ../../apps/parser/dist/DibParser$(GetExecutableSuffix) testing.dib spi console.dib spi math.dib spi "optionm'.dib" spi "am'.dib" spi "listm'.dib" spi util.dib spi physics.dib spi } | Invoke-Block
+{ . ../../apps/parser/dist/DibParser$(GetExecutableSuffix) testing.dib spi common.dib spi console.dib spi math.dib spi "optionm'.dib" spi "am'.dib" spi "listm'.dib" spi seq.dib spi util.dib spi physics.dib spi } | Invoke-Block
