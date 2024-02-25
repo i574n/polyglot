f9c726a3b42b3ad87fdd6f5632107770d863decd
프리마 <fc1943s@pm.me>
2024-02-25 14:38:06 -0400

    tests

---
+++ lib/rust/fable/fable-library-rust.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code,)]
+#![allow(non_camel_case_types,)]
+#![allow(non_snake_case,)]
+#![allow(non_upper_case_globals,)]
+#![allow(unreachable_code,)]
+#![allow(unused_attributes,)]
+#![allow(unused_imports,)]
+#![allow(unused_macros,)]
+#![allow(unused_parens,)]
+#![allow(unused_variables,)]
+mod module_63d44c74 { }
+pub use module_63d44c74::*;
