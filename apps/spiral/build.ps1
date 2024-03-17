2bd8eea0dd743090d2910f08432ed5d00180bc5e
프리마 <fc1943s@pm.me>
2024-03-17 13:37:50 -0400

    tests

--- apps/spiral/build.ps1
+++ apps/spiral/build.ps1
@@ -15,6 +15,12 @@ if (!$fast) {
 
 { . ../builder/dist/Builder$(GetExecutableSuffix) Supervisor.fs $($fast ? @("--runtime", ($IsWindows ? "win-x64" : "linux-x64")) : @()) --packages Argu FSharp.Control.AsyncSeq FSharp.Json Microsoft.AspNetCore.SignalR.Client System.CommandLine System.Reactive.Linq --modules lib/fsharp/Common.fs lib/fsharp/CommonFSharp.fs lib/fsharp/Threading.fs lib/fsharp/Async.fs lib/fsharp/AsyncSeq.fs lib/fsharp/Networking.fs lib/fsharp/Runtime.fs lib/fsharp/FileSystem.fs } | Invoke-Block
 
+if (!$fast) {
+    Invoke-Dib Eval.dib -Retries 3
+}
+
+{ . ../parser/dist/DibParser$(GetExecutableSuffix) Eval.dib fs } | Invoke-Block
+
 $targetDir = "../../target/polyglot/builder/Supervisor"
 
 if ($env:CI) {
