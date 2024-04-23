be7de73ba700fc6d033da1bf38edf912bbe22508
프리마 <fc1943s@pm.me>
2024-04-22 21:14:03 -0400

    tests

--- apps/builder/build.ps1
+++ apps/builder/build.ps1
@@ -9,11 +9,11 @@ $ErrorActionPreference = "Stop"
 
 
 $dibParserExe = "../parser/dist/DibParser$(_exe)"
-if (!(Test-Path $dibParserExe)) {
-    pwsh ../parser/build.ps1 -fast 1
-
+if ($fast -and (Test-Path $dibParserExe)) {
+    { . $dibParserExe Builder.dib fs } | Invoke-Block
+} else {
+    { dotnet repl --run ../parser/DibParser.dib --exit-after-run } | Invoke-Block -EnvironmentVariables @{ "ARGS" = "Builder.dib fs" }
 }
-{ . $dibParserExe Builder.dib fs } | Invoke-Block
 
 $runtime = $fast -or $env:CI ? @("--runtime", ($IsWindows ? "win-x64" : "linux-x64")) : @()
 $builderArgs = @("Builder.fs", [String]::Join(" ", $runtime), "--packages", "Argu", "FSharp.Control.AsyncSeq", "System.CommandLine", "System.Reactive.Linq", "--modules", [String]::Join(" ", $(GetFsxModules)), "lib/fsharp/Common.fs", "lib/fsharp/CommonFSharp.fs", "lib/fsharp/Async.fs", "lib/fsharp/AsyncSeq.fs", "lib/fsharp/Runtime.fs", "lib/fsharp/FileSystem.fs")
