8a9e5af3845f14908d8590cb6f6e6cee1d046a02
프리마 <fc1943s@pm.me>
2024-11-05 22:00:09 -0300

    tests

--- apps/spiral/temp/build.ps1
+++ apps/spiral/temp/build.ps1
@@ -1,30 +1,13 @@
 param(
-    $fast,
-    $SkipNotebook,
-    $SkipFsx,
-    $SkipPreBuild,
     $ScriptDir = $PSScriptRoot
 )
 Set-Location $ScriptDir
 $ErrorActionPreference = "Stop"
 . ../../../scripts/core.ps1
-. ../../../lib/spiral/lib.ps1
 
 
-$projectName = "cube"
+{ pwsh ./cube/build.ps1 } | Invoke-Block
 
-if (!$SkipPreBuild -and !$SkipFsx) {
-    if (!$SkipNotebook) {
-        { . ../dist/Supervisor$(_exe) --execute-command "../../../workspace/target/release/spiral_builder$(_exe) dib --path $projectName.dib" } | Invoke-Block -Retries 3
-    }
+{ pwsh ./extension/build.ps1 } | Invoke-Block
 
-    # { . ../../parser/dist/DibParser$(_exe) "$projectName.dib" spi } | Invoke-Block
-
-    # { . ../dist/Supervisor$(_exe) --build-file "$projectName.spi" "$projectName.fsx" } | Invoke-Block
-}
-
-if (!$fast) {
-    { pwsh ./extension/build.ps1 } | Invoke-Block
-
-    { pwsh ./test/build.ps1 } | Invoke-Block
-}
+{ pwsh ./test/build.ps1 } | Invoke-Block
