906ff38d3dde8749ad73dad3c3001be32f387cc9
프리마 <fc1943s@pm.me>
2025-03-27 16:10:03 -0400

    tests

--- lib/rust/fable/build.ps1
+++ lib/rust/fable/build.ps1
@@ -22,11 +22,11 @@ $rsPath = "$libPath/src/Seq.rs"
     -replace "\(&e\.get_Current", "(e.get_Current" `
     | Set-Content $rsPath
 
-$rsPath = "$libPath/src/FSharp.Core.rs"
-(Get-Content $rsPath) `
-    -replace "use crate::Native_::isNull;", "use crate::Native_::is_null;" `
-    -replace "isNull\(", "is_null(" `
-    | Set-Content $rsPath
+# $rsPath = "$libPath/src/FSharp.Core.rs"
+# (Get-Content $rsPath) `
+#     -replace "use crate::Native_::isNull;", "use crate::Native_::is_null;" `
+#     -replace "isNull\(", "is_null(" `
+#     | Set-Content $rsPath
 
 $rsPath = "$libPath/src/List.rs"
 (Get-Content $rsPath) `
@@ -36,25 +36,25 @@ $rsPath = "$libPath/src/List.rs"
     -replace " gen ", " f " `
     | Set-Content $rsPath
 
-$rsPath = "$libPath/src/Option.rs"
-(Get-Content $rsPath) `
-    -replace "use crate::Native_::isNull;", "use crate::Native_::is_null;" `
-    -replace "use crate::Native_::getNull;", "use crate::Native_::getZero;" `
-    -replace "isNull\(", "is_null(" `
-    -replace "getNull::<\(\)>\(\)", "getZero()" `
-    | Set-Content $rsPath
+# $rsPath = "$libPath/src/Option.rs"
+# (Get-Content $rsPath) `
+#     -replace "use crate::Native_::isNull;", "use crate::Native_::is_null;" `
+#     -replace "use crate::Native_::getNull;", "use crate::Native_::getZero;" `
+#     -replace "isNull\(", "is_null(" `
+#     -replace "getNull::<\(\)>\(\)", "getZero()" `
+#     | Set-Content $rsPath
 
-$rsPath = "$libPath/src/System.rs"
-(Get-Content $rsPath) `
-    -replace "use crate::String_::isEmpty;", "use crate::String_::isNullOrEmpty;" `
-    -replace "isEmpty", "isNullOrEmpty" `
-    | Set-Content $rsPath
+# $rsPath = "$libPath/src/System.rs"
+# (Get-Content $rsPath) `
+#     -replace "use crate::String_::isEmpty;", "use crate::String_::isNullOrEmpty;" `
+#     -replace "isEmpty", "isNullOrEmpty" `
+#     | Set-Content $rsPath
 
-$rsPath = "$libPath/src/System.Text.rs"
-(Get-Content $rsPath) `
-    -replace "use crate::String_::isEmpty;", "use crate::String_::isNullOrEmpty;" `
-    -replace "isEmpty", "isNullOrEmpty" `
-    | Set-Content $rsPath
+# $rsPath = "$libPath/src/System.Text.rs"
+# (Get-Content $rsPath) `
+#     -replace "use crate::String_::isEmpty;", "use crate::String_::isNullOrEmpty;" `
+#     -replace "isEmpty", "isNullOrEmpty" `
+#     | Set-Content $rsPath
 
 { cargo check } | Invoke-Block -Location $libPath -OnError Continue
 if (!$fast) {
