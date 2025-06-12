cd5844c668d19fc59ba0b5a7865746dea6d35240
프리마 <fc1943s@pm.me>
2025-06-12 06:51:15 -0400

    tests

--- lib/math/build.ps1
+++ lib/math/build.ps1
@@ -44,7 +44,7 @@ Write-Output "polyglot/lib/math/build.ps1 / path: $path"
 
 cargo fmt --
 
-{ cargo test --release } | Invoke-Block
+{ cargo test --release } | Invoke-Block -OnError Continue
 
 Write-Output "polyglot/lib/math/build.ps1 / `$targetDir: $targetDir / `$projectName: $projectName / `$env:CI:'$env:CI'"
 
