0580ceb6d218bbb35978eebe51ba263db422ef93
fc1943s <fc1943s@users.noreply.github.com>
2025-06-18 17:59:25 +0000

    deploy: a45129b77abd4d6c175a45197a3880857f0ff615

--- apps/chat/contract/chat_contract.rs
+++ apps/chat/contract/chat_contract.rs
@@ -609,24 +609,24 @@ mod module_31a2fc18 {
             v4: LrcPtr<Chat_contract::Mut4>,
             v5: Option<i64>,
         ) -> string {
-            let v569: u64 = near_sdk::env::block_timestamp();
-            let v594: Chat_contract::US2 =
+            let v638: u64 = near_sdk::env::block_timestamp();
+            let v663: Chat_contract::US2 =
                 defaultValue(Chat_contract::US2::US2_1, map(Chat_contract::method7(), v5));
-            let v607: u64 = (match &v594 {
-                Chat_contract::US2::US2_0(v594_0_0) => {
-                    (v569)
-                        - (match &v594 {
+            let v676: u64 = (match &v663 {
+                Chat_contract::US2::US2_0(v663_0_0) => {
+                    (v638)
+                        - (match &v663 {
                             Chat_contract::US2::US2_0(x) => x.clone(),
                             _ => unreachable!(),
                         } as u64)
                 }
-                _ => v569,
+                _ => v638,
             }) / 1000000000_u64;
-            let v608: u64 = (v607) % 60_u64;
-            let v610: u64 = ((v607) / 60_u64) % 60_u64;
-            let v612: u64 = ((v607) / 3600_u64) % 24_u64;
-            let v614: std::string::String = format!("{:02}:{:02}:{:02}", v612, v610, v608);
-            fable_library_rust::String_::fromString(v614)
+            let v677: u64 = (v676) % 60_u64;
+            let v679: u64 = ((v676) / 60_u64) % 60_u64;
+            let v681: u64 = ((v676) / 3600_u64) % 24_u64;
+            let v683: std::string::String = format!("{:02}:{:02}:{:02}", v681, v679, v677);
+            fable_library_rust::String_::fromString(v683)
         }
         pub fn method12() -> string {
             string("")
