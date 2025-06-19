4c321aee438a1883c2df0b8ac1ac243e135b9019
fc1943s <fc1943s@users.noreply.github.com>
2025-06-19 20:44:05 +0000

    deploy: 695f3ad9d14d2506f43a25915dc57ec40d471cd5

--- apps/spiral/temp/build.ps1
+++ apps/spiral/temp/build.ps1
@@ -13,10 +13,12 @@ $ErrorActionPreference = "Stop"
 { pwsh ./test/build.ps1 } | Invoke-Block
 
 
-if ($IsLinux) {
-    { sudo apt-get update } | Invoke-Block -Linux -Distro ubuntu
-    { sudo add-apt-repository ppa:thomas-schiex/blender } | Invoke-Block -Linux -Distro ubuntu -OnError Continue
-    { sudo apt install -y blender } | Invoke-Block -Linux -Distro ubuntu -OnError Continue
-}
+if (!$env:CI) {
+    if ($IsLinux) {
+        { sudo apt-get update } | Invoke-Block -Linux -Distro ubuntu
+        { sudo add-apt-repository ppa:thomas-schiex/blender } | Invoke-Block -Linux -Distro ubuntu -OnError Continue
+        { sudo apt install -y blender } | Invoke-Block -Linux -Distro ubuntu -OnError Continue
+    }
 
-{ pwsh ./blender/build.ps1 } | Invoke-Block
+    { pwsh ./blender/build.ps1 } | Invoke-Block
+}
