6c7775dd0432927a498ebc882c1a74745342f207
프리마 <fc1943s@pm.me>
2025-10-27 04:18:35 -0400

    tests

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,10 +3,6 @@ apt install -y snapd unzip
 snap install powershell --classic
 
 df -h
-apt-get remove -y '^ghc-8.*'
-apt-get remove -y '^llvm-.*'
-apt-get remove -y 'php.*'
-apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox mono-devel
 apt-get autoremove -y
 apt-get clean
 df -h
